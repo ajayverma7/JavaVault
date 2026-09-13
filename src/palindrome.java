abd2c3b60397f74ff9beb922287205b092d8f24e
Ajay Verma <av955356@gmail.com>
2026-09-13 13:41:38 +0530

    Add Palindrome Program

---
+++ src/palindrome.java
@@ -0,0 +1,32 @@
+package src;
+
+import java.util.*;
+
+public class palindrome {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in); // for user imput 
+
+        System.out.println("Enter a number to Check Palindrome:");
+        int original = sc.nextInt();
+        int number = original; // we will perform action on number 
+        
+        // reversing a number
+        int reversed = 0;
+        while(number>0){
+            int digit = number%10;
+            reversed = reversed*10 + digit;
+            number = number/10;
+        }
+
+        // checking original == reversed or not
+        if(original == reversed){
+            System.out.println("Entered Number is palinderome.");
+        }
+        else{
+            System.out.println("Entered Number is not Palindrome.");
+        }
+    }
+
+    
+    
+}
